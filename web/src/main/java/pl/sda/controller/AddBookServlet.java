8a9c52cddc128742b8be1a4e49fcff7b9cd81334
draxolini <draxolini@interia.pl>
2020-01-09 22:20:01 +0100

    Added three jsp files

--- web/src/main/java/pl/sda/controller/AddBookServlet.java
+++ web/src/main/java/pl/sda/controller/AddBookServlet.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
-@WebServlet("/book/add")
+@WebServlet("/book/addBook")
 public class AddBookServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
